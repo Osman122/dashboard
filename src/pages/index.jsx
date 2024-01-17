b0656599a72294c8662de565f2df6d70ee5c7827
Osman122 <osoconan1@gmail.com>
2024-01-17 03:02:12 +0200

    name

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 function Home() {
-  return <h1>Home Page</h1>
+  return(
+    <>
+    <h2>Hi Salma</h2>
+    <h5>kalty alsabane5</h5>
+    
+    </>
+  )
 }
 export default Home
